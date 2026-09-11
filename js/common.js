dcae457b1e9dbe63c58c2cdad61197e8d5bcef1c
wjdaldus598 <wjdaldus598@gmail.com>
2026-09-11 12:55:51 +0900

    2026-09-11 | feat: 공통 버튼 호버 시 도넛 갉아먹는 효과와 부스러기 추가

--- js/common.js
+++ js/common.js
@@ -20,11 +20,26 @@ AOS.init({
 });
 
 $(function () {
+  btnBite();
   storyDonutRoll();
   doStepScroll();
   rollSlide();
   crewFlow();
 });
+
+function btnBite() {
+  function rand(min, max) {
+    return (min + Math.random() * (max - min)).toFixed(1) + '%';
+  }
+
+  $('.btn-round').each(function () {
+    for (var i = 0; i < 6; i++) $(this).append('<span class="btn-crumb" aria-hidden="true"></span>');
+  }).on('mouseenter focusin', function () {
+    this.style.setProperty('--x1', rand(58, 82));
+    this.style.setProperty('--y2', rand(38, 62));
+    this.style.setProperty('--x3', rand(16, 40));
+  });
+}
 
 function crewFlow() {
   var $tracks = $('.sc-crew .crew-track');
